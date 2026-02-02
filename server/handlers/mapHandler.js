90c98b514638933f656b3603a94a9d01a5418ea9
el mehdi belfkih <elmehdibelfkih@gmail.com>
2026-02-03 00:46:30 +0100

    fixing map naming

--- server/handlers/mapHandler.js
+++ server/handlers/mapHandler.js
@@ -10,7 +10,7 @@ const __dirname = dirname(__filename);
 
 export function getMultiplayerMap() {
     const randomIndex = Math.floor(Math.random() * 10) + 1;
-    const mapFileName = `level${randomIndex}.json`;
+    const mapFileName = `map${randomIndex}.json`;
     const mapPath = join(__dirname, '../maps', mapFileName);
 
     const mapData = readFileSync(mapPath, 'utf-8');
